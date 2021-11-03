791528fae27879221b06099ddb2e7a6762f3553c
Matthias Emde <matthias.emde@tum.de>
2021-11-03 19:05:01 +0100

    first layout for db testing

--- app/src/main/java/de/practicetime/practicetime/MainActivity.kt
+++ app/src/main/java/de/practicetime/practicetime/MainActivity.kt
@@ -2,10 +2,20 @@ package de.practicetime.practicetime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.addSection).setOnClickListener {
+            val categoryId = findViewById<EditText>(R.id.categoryId).text
+            val duration = findViewById<EditText>(R.id.duration).text
+
+            Toast.makeText(this, "Played category $categoryId for ${duration}h.", Toast.LENGTH_SHORT).show()
+        }
     }
 }
