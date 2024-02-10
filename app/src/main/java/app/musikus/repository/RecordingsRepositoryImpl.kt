b38946fa88abcb32ec0c2fcd365b2920c7dcef6d
Matthias Emde <matthias@emdemail.de>
2024-02-10 17:45:35 +0100

    moved recordings repository functions to the IO dispatcher to make the main safe

--- app/src/main/java/app/musikus/repository/RecordingsRepositoryImpl.kt
+++ app/src/main/java/app/musikus/repository/RecordingsRepositoryImpl.kt
@@ -24,6 +24,7 @@ import app.musikus.di.IoScope
 import app.musikus.usecase.recordings.Recording
 import app.musikus.usecase.recordings.RecordingsRepository
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
@@ -55,94 +56,101 @@ class RecordingsRepositoryImpl(
     // source: https://stackoverflow.com/questions/55212362/how-to-decode-an-m4a-audio-on-android
     @OptIn(ExperimentalCoroutinesApi::class)
     override suspend fun getRawRecording(contentUri: Uri): Result<FloatArray> {
+        return withContext(Dispatchers.IO) {
 
-        val mediaExtractor = MediaExtractor()
+            val mediaExtractor = MediaExtractor()
 
-        Log.d("RecordingsRepository", "getRawRecording: $contentUri")
+            Log.d("RecordingsRepository", "getRawRecording: $contentUri")
 
-        val recordingBuffer = ArrayList<Float>()
-        var mediaFormat : MediaFormat? = null
+            val recordingBuffer = ArrayList<Float>()
+            var mediaFormat : MediaFormat? = null
 
-        mediaExtractor.setDataSource(application, contentUri, null)
+            mediaExtractor.setDataSource(application, contentUri, null)
 
-        repeat(mediaExtractor.trackCount) { trackIndex ->
-            mediaFormat = mediaExtractor.getTrackFormat(trackIndex)
-            if(mediaFormat?.getString(MediaFormat.KEY_MIME)?.startsWith("audio/") == true) {
-                mediaExtractor.selectTrack(trackIndex)
-                return@repeat
+            repeat(mediaExtractor.trackCount) { trackIndex ->
+                mediaFormat = mediaExtractor.getTrackFormat(trackIndex)
+                if(mediaFormat?.getString(MediaFormat.KEY_MIME)?.startsWith("audio/") == true) {
+                    mediaExtractor.selectTrack(trackIndex)
+                    return@repeat
+                }
             }
-        }
 
-        mediaFormat?.let { nonNullMediaFormat ->
+            // make sure, the mediaFormat is not null
+            val nonNullMediaFormat = mediaFormat
+                ?: return@withContext Result.failure(Exception("No audio track found"))
+
+            Log.d("RecordingsRepository", "MediaFormat: $nonNullMediaFormat")
+
             // we get the mime type from the mediaFormat
-            val mimeType = nonNullMediaFormat.getString(MediaFormat.KEY_MIME) ?: return Result.failure(Exception("No mime type found"))
+            val mimeType = nonNullMediaFormat.getString(MediaFormat.KEY_MIME)
+                ?: return@withContext Result.failure(Exception("No mime type found"))
 
             Log.d("RecordingsRepository", "Mime type: $mimeType")
 
             val decoder = MediaCodec.createDecoderByType(mimeType)
 
-            return withContext(ioScope.coroutineContext) {
-                suspendCancellableCoroutine { coroutine ->
+            return@withContext suspendCancellableCoroutine { coroutine ->
 
-                    coroutine.invokeOnCancellation {
-                        decoder.release()
-                        mediaExtractor.release()
+                coroutine.invokeOnCancellation {
+                    Log.d("RecordingsRepository", "Cancellation invoked")
+                    mediaExtractor.release()
+                    decoder.stop()
+                    decoder.release()
+                }
+
+                decoder.setCallback(object : MediaCodec.Callback() {
+                    override fun onInputBufferAvailable(codec: MediaCodec, index: Int) {
+                        val inputBuffer = codec.getInputBuffer(index) ?: return
+                        val sampleSize = mediaExtractor.readSampleData(inputBuffer, 0)
+                        if(sampleSize < 0) {
+                            codec.queueInputBuffer(index, 0, 0, 0, MediaCodec.BUFFER_FLAG_END_OF_STREAM)
+                        } else {
+                            codec.queueInputBuffer(index, 0, sampleSize, mediaExtractor.sampleTime, 0)
+                            mediaExtractor.advance()
+                        }
                     }
 
-                    decoder.setCallback(object : MediaCodec.Callback() {
-                        override fun onInputBufferAvailable(codec: MediaCodec, index: Int) {
-                            val inputBuffer = codec.getInputBuffer(index) ?: return
-                            val sampleSize = mediaExtractor.readSampleData(inputBuffer, 0)
-                            if(sampleSize < 0) {
-                                codec.queueInputBuffer(index, 0, 0, 0, MediaCodec.BUFFER_FLAG_END_OF_STREAM)
-                            } else {
-                                codec.queueInputBuffer(index, 0, sampleSize, mediaExtractor.sampleTime, 0)
-                                mediaExtractor.advance()
-                            }
+                    override fun onOutputBufferAvailable(codec: MediaCodec, index: Int, info: MediaCodec.BufferInfo) {
+                        val outputBuffer = codec.getOutputBuffer(index) ?: return
+
+                        if (info.flags and MediaCodec.BUFFER_FLAG_END_OF_STREAM != 0) {
+                            Log.d("RecordingsRepository", "End of stream reached")
+                            coroutine.resume(
+                                value = Result.success(recordingBuffer.toFloatArray()),
+                                onCancellation = null
+                            )
+                            return
                         }
 
-                        override fun onOutputBufferAvailable(codec: MediaCodec, index: Int, info: MediaCodec.BufferInfo) {
-                            val outputBuffer = codec.getOutputBuffer(index) ?: return
-
-                            if (info.flags and MediaCodec.BUFFER_FLAG_END_OF_STREAM != 0) {
-                                Log.d("RecordingsRepository", "End of stream reached")
-                                coroutine.resume(
-                                    value = Result.success(recordingBuffer.toFloatArray()),
-                                    onCancellation = null
-                                )
-                                return
-                            }
-
-                            for (i in 0 until info.size step 2) {
-                                recordingBuffer.add (outputBuffer.getShort(i).toFloat())
-                            }
-
-                            outputBuffer.clear()
-                            codec.releaseOutputBuffer(index, false)
+                        for (i in 0 until info.size step 2) {
+                            recordingBuffer.add (outputBuffer.getShort(i).toFloat())
                         }
 
-                        override fun onError(codec: MediaCodec, e: MediaCodec.CodecException) {
-                            Log.e("RecordingsRepository", "Error in mediacodec callback: $e")
-                            coroutine.resume(Result.failure(e), onCancellation = null)
-                        }
+                        outputBuffer.clear()
+                        codec.releaseOutputBuffer(index, false)
+                    }
 
-                        override fun onOutputFormatChanged(codec: MediaCodec, format: MediaFormat) {
-                            Log.d("RecordingsRepository", "Output format changed: $format")
-                        }
-                    })
+                    override fun onError(codec: MediaCodec, e: MediaCodec.CodecException) {
+                        Log.e("RecordingsRepository", "Error in mediacodec callback: $e")
+                        coroutine.resume(Result.failure(e), onCancellation = null)
+                    }
 
-                    /**
-                     *  ------------------- Configure and start the decoder -----------------------
-                     */
-                    decoder.configure(nonNullMediaFormat, null, null, 0)
-                    decoder.start()
-                }
+                    override fun onOutputFormatChanged(codec: MediaCodec, format: MediaFormat) {
+                        Log.d("RecordingsRepository", "Output format changed: $format")
+                    }
+                })
+
+                /**
+                 *  ------------------- Configure and start the decoder -----------------------
+                 */
+                decoder.configure(nonNullMediaFormat, null, null, 0)
+                decoder.start()
             }
-
-        } ?: return Result.failure(Exception("No audio track found"))
+        }
     }
 
-    private fun <T> subscribeTo(query: () -> T): Flow<T> {
+    // query has to be non-blocking
+    private fun <T> subscribeTo(query: suspend () -> T): Flow<T> {
         val notify = MutableSharedFlow<String>()
 
         var observer: ContentObserver? = null
@@ -171,44 +179,46 @@ class RecordingsRepositoryImpl(
         }
     }
 
-    private fun getRecordings() : List<Recording> {
+    private suspend fun getRecordings() : List<Recording> {
         val recordingList = mutableListOf<Recording>()
 
-        contentResolver.query(
-            collection,
-            arrayOf(
-                MediaStore.Audio.Media._ID,
-                MediaStore.Audio.Media.DISPLAY_NAME,
-                MediaStore.Audio.Media.DURATION,
-                MediaStore.Audio.Media.DATE_ADDED,
-            ),
-            "${MediaStore.Audio.Media.ALBUM} = 'Musikus'",
-            null,
-            "${MediaStore.Audio.Media.DATE_ADDED} DESC"
-        )?.use { cursor ->
-            val idColumn = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media._ID)
-            val displayNameColumn = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DISPLAY_NAME)
-            val durationColumn = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DURATION)
-            val dateColumn = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DATE_ADDED)
+        withContext(Dispatchers.IO) {
+            contentResolver.query(
+                collection,
+                arrayOf(
+                    MediaStore.Audio.Media._ID,
+                    MediaStore.Audio.Media.DISPLAY_NAME,
+                    MediaStore.Audio.Media.DURATION,
+                    MediaStore.Audio.Media.DATE_ADDED,
+                ),
+                "${MediaStore.Audio.Media.ALBUM} = 'Musikus'",
+                null,
+                "${MediaStore.Audio.Media.DATE_ADDED} DESC"
+            )?.use { cursor ->
+                val idColumn = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media._ID)
+                val displayNameColumn = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DISPLAY_NAME)
+                val durationColumn = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DURATION)
+                val dateColumn = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DATE_ADDED)
 
-            while(cursor.moveToNext()) {
-                val id = cursor.getLong(idColumn)
-                val displayName = cursor.getString(displayNameColumn)
-                val duration = cursor.getInt(durationColumn)
-                val date = cursor.getLong(dateColumn)
-                val contentUri = ContentUris.withAppendedId(
-                    MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
-                    id
-                )
-                recordingList.add(
-                    Recording(
-                        mediaItem = MediaItem.fromUri(contentUri),
-                        title = displayName,
-                        duration = duration.milliseconds,
-                        date = ZonedDateTime.ofInstant(Instant.ofEpochSecond(date), ZoneId.systemDefault()),
-                        contentUri = contentUri
+                while(cursor.moveToNext()) {
+                    val id = cursor.getLong(idColumn)
+                    val displayName = cursor.getString(displayNameColumn)
+                    val duration = cursor.getInt(durationColumn)
+                    val date = cursor.getLong(dateColumn)
+                    val contentUri = ContentUris.withAppendedId(
+                        MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+                        id
                     )
-                )
+                    recordingList.add(
+                        Recording(
+                            mediaItem = MediaItem.fromUri(contentUri),
+                            title = displayName,
+                            duration = duration.milliseconds,
+                            date = ZonedDateTime.ofInstant(Instant.ofEpochSecond(date), ZoneId.systemDefault()),
+                            contentUri = contentUri
+                        )
+                    )
+                }
             }
         }
 
