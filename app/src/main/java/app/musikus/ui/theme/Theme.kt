0494e7131211e654f5b5a3d36792a0dbddbd2651
Matthias Emde <matthias@emdemail.de>
2024-03-08 20:55:55 +0100

    Add legacy color scheme

--- app/src/main/java/app/musikus/ui/theme/Theme.kt
+++ app/src/main/java/app/musikus/ui/theme/Theme.kt
@@ -13,10 +13,8 @@ import android.os.Build
 import android.view.WindowManager
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.ReadOnlyComposable
 import androidx.compose.runtime.SideEffect
@@ -31,69 +29,20 @@ import androidx.core.view.WindowCompat
 import app.musikus.datastore.ColorSchemeSelections
 import app.musikus.datastore.ThemeSelections
 
-private val LightColors = lightColorScheme(
-    primary = md_theme_light_primary,
-    onPrimary = md_theme_light_onPrimary,
-    primaryContainer = md_theme_light_primaryContainer,
-    onPrimaryContainer = md_theme_light_onPrimaryContainer,
-    secondary = md_theme_light_secondary,
-    onSecondary = md_theme_light_onSecondary,
-    secondaryContainer = md_theme_light_secondaryContainer,
-    onSecondaryContainer = md_theme_light_onSecondaryContainer,
-    tertiary = md_theme_light_tertiary,
-    onTertiary = md_theme_light_onTertiary,
-    tertiaryContainer = md_theme_light_tertiaryContainer,
-    onTertiaryContainer = md_theme_light_onTertiaryContainer,
-    error = md_theme_light_error,
-    errorContainer = md_theme_light_errorContainer,
-    onError = md_theme_light_onError,
-    onErrorContainer = md_theme_light_onErrorContainer,
-    background = md_theme_light_background,
-    onBackground = md_theme_light_onBackground,
-    surface = md_theme_light_surface,
-    onSurface = md_theme_light_onSurface,
-    surfaceVariant = md_theme_light_surfaceVariant,
-    onSurfaceVariant = md_theme_light_onSurfaceVariant,
-    outline = md_theme_light_outline,
-    inverseOnSurface = md_theme_light_inverseOnSurface,
-    inverseSurface = md_theme_light_inverseSurface,
-    inversePrimary = md_theme_light_inversePrimary,
-    surfaceTint = md_theme_light_surfaceTint,
-    outlineVariant = md_theme_light_outlineVariant,
-    scrim = md_theme_light_scrim,
-)
 
+/** Library item colors */
 
-private val DarkColors = darkColorScheme(
-    primary = md_theme_dark_primary,
-    onPrimary = md_theme_dark_onPrimary,
-    primaryContainer = md_theme_dark_primaryContainer,
-    onPrimaryContainer = md_theme_dark_onPrimaryContainer,
-    secondary = md_theme_dark_secondary,
-    onSecondary = md_theme_dark_onSecondary,
-    secondaryContainer = md_theme_dark_secondaryContainer,
-    onSecondaryContainer = md_theme_dark_onSecondaryContainer,
-    tertiary = md_theme_dark_tertiary,
-    onTertiary = md_theme_dark_onTertiary,
-    tertiaryContainer = md_theme_dark_tertiaryContainer,
-    onTertiaryContainer = md_theme_dark_onTertiaryContainer,
-    error = md_theme_dark_error,
-    errorContainer = md_theme_dark_errorContainer,
-    onError = md_theme_dark_onError,
-    onErrorContainer = md_theme_dark_onErrorContainer,
-    background = md_theme_dark_background,
-    onBackground = md_theme_dark_onBackground,
-    surface = md_theme_dark_surface,
-    onSurface = md_theme_dark_onSurface,
-    surfaceVariant = md_theme_dark_surfaceVariant,
-    onSurfaceVariant = md_theme_dark_onSurfaceVariant,
-    outline = md_theme_dark_outline,
-    inverseOnSurface = md_theme_dark_inverseOnSurface,
-    inverseSurface = md_theme_dark_inverseSurface,
-    inversePrimary = md_theme_dark_inversePrimary,
-    surfaceTint = md_theme_dark_surfaceTint,
-    outlineVariant = md_theme_dark_outlineVariant,
-    scrim = md_theme_dark_scrim,
+val libraryItemColors = listOf(
+    Color(0xFFf7a397),
+    Color(0xFFE03F54),
+    Color(0xFFFF8F33),
+    Color(0xFFFFC233),
+    Color(0xFF997099),
+    Color(0xFF5474BF),
+    Color(0xFF80995C),
+    Color(0xFF29CCAE),
+    Color(0xFF748EA7),
+    Color(0xFFA8684C)
 )
 
 @Composable
@@ -110,17 +59,17 @@ fun MusikusTheme(
 
     val colorSchemeDarkOrLight = when(colorScheme) {
         ColorSchemeSelections.MUSIKUS -> {
-            if (useDarkTheme) DarkColors else LightColors
+            if (useDarkTheme) DarkColorScheme else LightColorScheme
         }
         ColorSchemeSelections.LEGACY -> {
-            if (useDarkTheme) DarkColors else LightColors
+            if (useDarkTheme) LegacyDarkColorScheme else LegacyLightColorScheme
         }
         ColorSchemeSelections.DYNAMIC -> {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
                 val context = LocalContext.current
                 if (useDarkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
             } else {
-                if (useDarkTheme) DarkColors else LightColors
+                if (useDarkTheme) DarkColorScheme else LightColorScheme
             }
         }
     }
@@ -153,7 +102,7 @@ fun MusikusTheme(
                 ) {
                     Color.Transparent.toArgb()
                 } else {
-                    LightColors.inverseSurface.toArgb()
+                    LightColorScheme.inverseSurface.toArgb()
                 }
             }
 
