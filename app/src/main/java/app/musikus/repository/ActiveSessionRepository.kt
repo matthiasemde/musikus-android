5a559fd17e357e0e5c0fa3f0def9eb10e857f145
Michael Prommersberger <github@mipro.dev>
2024-03-10 16:28:37 +0100

    add ActiveSessionRepository

---
+++ app/src/main/java/app/musikus/repository/ActiveSessionRepository.kt
@@ -0,0 +1,19 @@
+package app.musikus.repository
+
+import app.musikus.usecase.activesession.ActiveSessionRepository
+import app.musikus.usecase.activesession.SessionState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+
+class ActiveSessionRepositoryImpl () : ActiveSessionRepository {
+
+    private val sessionState = MutableStateFlow<SessionState?>(null)
+
+    override suspend fun setSessionState(sessionState: SessionState) {
+        this.sessionState.update { sessionState }
+    }
+
+    override fun getSessionState() = sessionState.asStateFlow()
+}
