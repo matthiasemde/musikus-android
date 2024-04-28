881fcd6657fb833e6ba65bef9fb42ead77f8bfff
Michael Prommersberger <github@mipro.dev>
2024-04-28 13:04:14 +0200

    add dummy layout to recorder

--- app/src/main/java/app/musikus/ui/activesession/recorder/Recorder.kt
+++ app/src/main/java/app/musikus/ui/activesession/recorder/Recorder.kt
@@ -10,6 +10,12 @@ package app.musikus.ui.activesession.recorder
 
 import android.util.Log
 import android.widget.Toast
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
@@ -18,11 +24,13 @@ import androidx.compose.runtime.mutableLongStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import app.musikus.ui.components.ExceptionHandler
+import app.musikus.ui.theme.dimensions
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.isActive
 
@@ -37,6 +45,16 @@ fun RecorderUi(
     val eventHandler = viewModel::onUiEvent
 
     val context = LocalContext.current
+
+    Column (Modifier.fillMaxWidth()) {
+        Box (Modifier.fillMaxWidth().height(MaterialTheme.dimensions.toolsHeaderHeight)){
+            Text(modifier = Modifier.align(Alignment.Center), text = "Recorder Header")
+        }
+        Box (Modifier.fillMaxWidth().height(MaterialTheme.dimensions.toolsBodyHeight)){
+            Text(modifier = Modifier.align(Alignment.Center), text = "Recorder Body")
+        }
+
+    }
 
     /**
      * Exception handling
