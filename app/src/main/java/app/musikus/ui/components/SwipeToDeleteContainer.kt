e5959347480a92b5d2bbfa1eaeb13aaee2aebd07
Michael Prommersberger <github@mipro.dev>
2024-05-17 14:48:48 +0200

    move license header in the correct place

--- app/src/main/java/app/musikus/ui/components/SwipeToDeleteContainer.kt
+++ app/src/main/java/app/musikus/ui/components/SwipeToDeleteContainer.kt
@@ -1,5 +1,3 @@
-package app.musikus.ui.components
-
 /*
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -8,6 +6,8 @@ package app.musikus.ui.components
  * Copyright (c) 2024 Michael Prommersberger
  *
  */
+
+package app.musikus.ui.components
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.animateColorAsState
