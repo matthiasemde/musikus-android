34923edd56019f667e59ca31cf9d798d77a1f5c2
Matthias Emde <matthias@emdemail.de>
2024-08-31 00:03:16 +0200

    update gradle and dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -110,16 +110,16 @@ tasks.withType<Test> {
 }
 
 dependencies {
-    val bomVersion = "2024.04.01"
+    val bomVersion = "2024.08.00"
 
-    val lifecycleVersion = "2.7.0"
+    val lifecycleVersion = "2.8.4"
     val roomVersion = "2.6.1"
     val navVersion = "2.7.7"
     val daggerHiltVersion = "2.49"
     val kotlinCoroutineVersion = "1.7.3"
     val mockkVersion = "1.13.9"
-    val media3Version = "1.3.0"
-    val activityVersion = "1.9.0"
+    val media3Version = "1.4.1"
+    val activityVersion = "1.9.1"
 
     implementation("androidx.activity:activity-ktx:$activityVersion")
 
@@ -179,12 +179,12 @@ dependencies {
     ksp("com.google.dagger:hilt-compiler:$daggerHiltVersion")
 
     // Data store
-    implementation("androidx.datastore:datastore-preferences:1.0.0")
+    implementation("androidx.datastore:datastore-preferences:1.1.1")
 
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.core:core-ktx:1.13.1")
 
     // Backport java.Time to SDK < 23
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.1")
 
     // Media Playback
     // For media playback using ExoPlayer
@@ -201,14 +201,14 @@ dependencies {
     androidTestImplementation("io.mockk:mockk-agent:$mockkVersion")
 
     // Local unit tests
-    testImplementation("androidx.test:core:1.5.0")
+    testImplementation("androidx.test:core:1.6.1")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.0")
 
     testImplementation("androidx.arch.core:core-testing:2.2.0")
     testImplementation("com.google.truth:truth:1.1.3")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.6.4")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.6.8")
 
     // Instrumentation tests
     androidTestImplementation("com.google.dagger:hilt-android-testing:$daggerHiltVersion")
@@ -216,11 +216,11 @@ dependencies {
     androidTestImplementation("junit:junit:4.13.2")
     androidTestImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:$kotlinCoroutineVersion")
     androidTestImplementation("androidx.arch.core:core-testing:2.2.0")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.6.4")
-    androidTestImplementation("androidx.test:core-ktx:1.5.0")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test:runner:1.5.2")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.6.8")
+    androidTestImplementation("androidx.test:core-ktx:1.6.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test:runner:1.6.2")
     androidTestImplementation("com.google.truth:truth:1.1.3")
     androidTestImplementation("android.arch.persistence.room:testing:1.1.1")
 
