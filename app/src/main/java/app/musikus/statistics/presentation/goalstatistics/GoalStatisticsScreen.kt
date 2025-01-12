043a460152bac441c8f9b4c294ada0b34ebafaf5
Matthias Emde <matthias@emdemail.de>
2025-01-13 00:07:24 +0100

    quickly improve look and feel of goal card

--- app/src/main/java/app/musikus/statistics/presentation/goalstatistics/GoalStatisticsScreen.kt
+++ app/src/main/java/app/musikus/statistics/presentation/goalstatistics/GoalStatisticsScreen.kt
@@ -40,7 +40,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign.Companion.End
 import androidx.compose.ui.text.style.TextOverflow
@@ -226,9 +225,7 @@ fun GoalStatisticsGoalSelector(
                             progress = { animatedProgress },
                             modifier = Modifier.matchParentSize(),
                             color = color,
-                            strokeCap = StrokeCap.Round,
                             drawStopIndicator = {},
-                            gapSize = 0.dp,
                         )
                     }
                     AnimatedVisibility(
